f224ad24fb5bd0b90bab0f78b46472509e3ad6ad
Aayush Shakya <aayushshakya2000@gmail.com>
2023-11-23 15:00:26 +0545

    Project Completed

---
+++ src/main/java/com/example/javafxdemo/controller/Correctanswer.java
@@ -0,0 +1,145 @@
+package com.example.javafxdemo.controller;
+
+public class Correctanswer {
+    public static void main(String[] args) {
+        QuizQuestion question1 = new QuizQuestion("What is the traditional dance of the Malay culture in Malaysia? ?", "Joget", "Silat", "Zapin", "Dikir Barat", "Joget");
+        question1.setCorrectAnswer("Joget");
+
+        QuizQuestion question2 = new QuizQuestion("What is the main religion practiced in Malaysia?", "Islam", "Buddhism", "Hinduism", "Christianity", "Islam");
+        question2.setCorrectAnswer("Islam");
+
+        QuizQuestion question3 = new QuizQuestion("Which iconic twin towers are a famous landmark in Kuala Lumpur, Malaysia?", "Petronas Twin Towers", "Kuala Lumpur Tower", "Langkawi Sky Bridge", "Genting Highlands", "Petronas Twin Towers");
+        question3.setCorrectAnswer("Petronas Twin Towers");
+
+        QuizQuestion question4 = new QuizQuestion("WWhich Malaysian state is known as the Land Below the Wind", "Sabah", "Perak", "Selangor", "Sarawak", "Sabah");
+        question4.setCorrectAnswer("Sabah");
+
+        QuizQuestion question5 = new QuizQuestion("What is the traditional clothing worn by Malay women in Malaysia? ", "Baju Kurung", "Kebaya", "Sarong", "Cheongsam", "Baju Kurung");
+        question5.setCorrectAnswer("Baju Kurung");
+
+        QuizQuestion question6 = new QuizQuestion("Which Malaysian island is famous for its clear waters and diving spots? ", "Tioman", "Langkawi", "Penang", "Perhentian", "Tioman");
+        question6.setCorrectAnswer("Tioman");
+
+        QuizQuestion question7 = new QuizQuestion("What is the popular Malaysian dish made from flat rice noodles, shrimp, and chili paste?", "Laksa", "Nasi Lemak", "Roti Canai", "Mee Goreng", "Laksa");
+        question7.setCorrectAnswer("Laksa");
+
+        QuizQuestion question8 = new QuizQuestion("What is the name of the national park in Malaysia known for its wildlife and rainforests? ", "Taman Negara National Park", "Gunung Mulu National Park", "Bako National Park", "Kinabalu Park", "Taman Negara National Park");
+        question8.setCorrectAnswer("Taman Negara National Park");
+
+        QuizQuestion question9 = new QuizQuestion("Which Malaysian city is known for its historic colonial architecture and a UNESCO World Heritage Site?", "Malacca", "Penang", "Kuala Lumpur", "Johor Bahru", "Malacca");
+        question9.setCorrectAnswer("Malacca");
+
+        QuizQuestion question10 = new QuizQuestion("What is the name of Malaysia's highest mountain?", "Mount Kinabalu", "Mount Murud", "Mount Tahan", "Mount Mulu", "Mount Kinabalu");
+        question10.setCorrectAnswer("Mount Kinabalu");
+
+        QuizQuestion question11 = new QuizQuestion("What is the official language of Singapore?", "Malay", "Mandarin", "Tamil", "English", "Malay");
+        question11.setCorrectAnswer("Malay");
+
+        QuizQuestion question12 = new QuizQuestion("What is the name of the river that flows through Singapore's central business district?", "Singapore River", "Kallang River", "Rochor River", "Marina River", "Singapore River");
+        question12.setCorrectAnswer("Singapore River");
+
+        QuizQuestion question13 = new QuizQuestion("Which iconic building is often referred to as the Lion City?", "Marina Bay Sands", "Raffles Hotel", "The Interlace", "Lion Tower", "Marina Bay Sands");
+        question13.setCorrectAnswer("Marina Bay Sands");
+
+        QuizQuestion question14 = new QuizQuestion("In which year did Singapore gain independence from Malaysia?", "1965", "1959", "1971", "1963", "1965");
+        question14.setCorrectAnswer("1965");
+
+        QuizQuestion question15 = new QuizQuestion("What is the name of the popular hawker center in Singapore known for its diverse food stalls?", "Maxwell Food Centre", "Lau Pa Sat", "Chinatown Food Street", "Newton Food Hub", "Maxwell Food Centre");
+        question15.setCorrectAnswer("Maxwell Food Centre");
+
+        QuizQuestion question16 = new QuizQuestion("Who is the founding father and first Prime Minister of Singapore?", "Lee Kuan Yew", "Goh Chok Tong", "Lee Hsien Loong", "Tony Tan", "Lee Kuan Yew");
+        question16.setCorrectAnswer("Lee Kuan Yew");
+
+        QuizQuestion question17 = new QuizQuestion("What is the national flower of Singapore?", "Orchid", "Hibiscus", "Jasmine", "Sunflower", "Orchid");
+        question17.setCorrectAnswer("Orchid");
+
+        QuizQuestion question18 = new QuizQuestion("What is the significance of National Day in Singapore?", "Independence Day", "Republic Day", "Constitution Day", "Founding Day", "Independence Day");
+        question18.setCorrectAnswer("Independence Day");
+
+        QuizQuestion question19 = new QuizQuestion("Name the cultural district in Singapore known for its colorful buildings and street art.", "Haji Lane", "Chinatown", "Little India", "Kampong Glam", "Haji Lane");
+        question19.setCorrectAnswer("Haji Lane");
+
+        QuizQuestion question20 = new QuizQuestion("Which is the oldest museum in Singapore, known for its historical and cultural artifacts?", "National Museum of Singapore", "ArtScience Museum", "Singapore Art Museum", "National Gallery Singapore", "National Museum of Singapore");
+        question20.setCorrectAnswer("National Museum of Singapore");
+
+        QuizQuestion question21 = new QuizQuestion("What is the capital city of Thailand?", "Bangkok", "Phnom Penh", "Hanoi", "Jakarta", "Bangkok");
+        question21.setCorrectAnswer("Bangkok");
+
+        QuizQuestion question22 = new QuizQuestion("What is the official language of Thailand?", "Thai", "Mandarin", "Khmer", "Lao", "Thai");
+        question22.setCorrectAnswer("Thai");
+
+        QuizQuestion question23 = new QuizQuestion("Which Thai city is famous for its ancient temples, including the Wat Phra Singh and Wat Chedi Luang, reflecting the rich cultural heritage of the country?", "Chiang Mai", "Chiang Rai", "Ayutthaya", "Sukhothai", "Chiang Mai");
+        question23.setCorrectAnswer("Chiang Mai");
+
+        QuizQuestion question24 = new QuizQuestion("What is the traditional Thai greeting often accompanied by a bow?", "Sawadee Krub/Ka", "Aloha", "Konnichiwa", "Namaste", "Sawadee Krub/Ka");
+        question24.setCorrectAnswer("Sawadee Krub/Ka");
+
+        QuizQuestion question25 = new QuizQuestion("What is the currency used in Thailand?", "Baht", "Rupiah", "Ringgit", "Dong", "Baht");
+        question25.setCorrectAnswer("Baht");
+
+        QuizQuestion question26 = new QuizQuestion("Which river flows through the heart of Bangkok, the capital of Thailand?", "Chao Phraya River", "Mekong River", "Yangtze River", "Irrawaddy River", "Chao Phraya River");
+        question26.setCorrectAnswer("Chao Phraya River");
+
+        QuizQuestion question27 = new QuizQuestion("Which national park in Thailand is renowned for its diverse wildlife, lush rainforests, and stunning waterfalls, making it a popular destination for nature enthusiasts and hikers?", "Khao Sok National Park", "Erawan National Park", "Doi Inthanon National Park", "Khao Yai National Park", "Khao Sok National Park");
+        question27.setCorrectAnswer("Khao Sok National Park");
+
+        QuizQuestion question28 = new QuizQuestion("What is the significance of Songkran, the Thai New Year festival?", "Water Festival", "Lantern Festival", "Flower Festival", "Firecracker Festival", "Water Festival");
+        question28.setCorrectAnswer("Water Festival");
+
+        QuizQuestion question29 = new QuizQuestion("Which traditional Thai massage technique is known for its therapeutic effects, combining acupressure, Indian Ayurvedic principles, and assisted yoga postures?", "Thai Massage", "Shiatsu", "Reiki", "Swedish Massage", "Thai Massage");
+        question29.setCorrectAnswer("Thai Massage");
+
+        QuizQuestion question30 = new QuizQuestion("Who is the current monarch of Thailand as of your knowledge cutoff date?", "King Rama X", "King Rama IX", "King Vajiralongkorn", "King Bhumibol Adulyadej", "King Rama X");
+        question30.setCorrectAnswer("King Rama X");
+    }
+
+    public static class QuizQuestion {
+        private String question;
+        private String correctAnswer;
+        private String option1;
+        private String option2;
+        private String option3;
+        private String option4;
+
+        public QuizQuestion(String question, String correctAnswer, String option1, String option2, String option3, String option4) {
+            this.question = question;
+            this.correctAnswer = correctAnswer;
+            this.option1 = option1;
+            this.option2 = option2;
+            this.option3 = option3;
+            this.option4 = option4;
+        }
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public String getCorrectAnswer() {
+            return correctAnswer;
+        }
+
+        public String getOption1() {
+            return option1;
+        }
+
+        public String getOption2() {
+            return option2;
+        }
+
+        public String getOption3() {
+            return option3;
+        }
+
+        public String getOption4() {
+            return option4;
+        }
+
+        public void setCorrectAnswer(String correctAnswer) {
+            this.correctAnswer = correctAnswer;
+        }
+    }
+}
+
+
+
+
---
+++ src/main/java/com/example/javafxdemo/controller/QuizQuestion.java
@@ -0,0 +1,45 @@
+package com.example.javafxdemo.controller;
+
+public  class QuizQuestion {
+    private String question;
+    private String correctAnswer;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+
+    public QuizQuestion(String question, String correctAnswer, String option1, String option2, String option3, String option4) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    }
+
